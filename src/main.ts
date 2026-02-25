333e2d175fbb789ba8438a6daf35be30a903cf31
JhonattanAron <aron.cachago@gmail.com>
2026-02-25 16:42:48 -0500

    1.1.4

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,18 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.enableCors({
-    origin: ["http://localhost:3002", "https://dekorean.aurentric.com"],
+    origin: [
+      "http://localhost:3000", // desarrollo
+      "https://dekorans.es",
+      "https://dekorans.info",
+      "https://dekorans.shop",
+    ],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
+
   await app.listen(process.env.PORT ?? 8082);
 }
 bootstrap();
