5547f696a8dc15df97699ebb8163ac1bba4725e5
JhonattanAron <aron.cachago@gmail.com>
2026-03-09 17:11:08 -0500

    2.2.2

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as bodyParser from "body-parser";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,9 @@ async function bootstrap() {
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
+
+  app.use(bodyParser.json({ limit: "10mb" }));
+  app.use(bodyParser.urlencoded({ limit: "10mb", extended: true }));
 
   await app.listen(process.env.PORT ?? 8082);
 }
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -50,6 +50,8 @@ export class ProductsController {
 
   @Patch(":id")
   update(@Param("id") id: string, @Body() updateProductDto: UpdateProductDto) {
+    console.log(updateProductDto);
+
     return this.productsService.update(id, updateProductDto);
   }
 
